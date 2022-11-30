ef8bd44e148d3b3ce2c95bd5fc3e055aef4aab2b
John Deen Bucog <JDBucog@yahoo.com>
2022-11-30 15:48:04 -0800

    setup CartItem Component, add styles, and props

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const CartItem = ({id, title, price, image, amount}) => {
-  
+import React from "react"
+import { ChevronDown, ChevronUp } from "../icons"
+const CartItem = ({ id, title, price, image, amount }) => {
   return (
-    <div>CartItem</div>
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">Remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
   )
 }
 
