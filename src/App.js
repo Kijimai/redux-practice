8a1198fd60f9b22771b1213ffc17cd0467addd4c
John Deen Bucog <JDBucog@yahoo.com>
2022-11-30 18:06:11 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import CartContainer from "./components/CartContainer"
 import Modal from "./components/Modal"
 import { useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
-import { calculateTotals } from "./features/cart/cartSlice"
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice"
 
 function App() {
   const dispatch = useDispatch()
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
   const { isShowing } = useSelector((state) => state.modal)
+
+  useEffect(() => {
+    dispatch(getCartItems('first param value in thunk'))
+  }, [])
 
   useEffect(() => {
     dispatch(calculateTotals())
     // eslint-disable-next-line
   }, [cartItems])
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit"
-import cartItems from "../../cartItems"
+import axios from "axios"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { showModal } from "../modal/modalSlice"
+// import cartItems from "../../cartItems"
+const url = "https://course-api.com/react-useReducer-cart-projects"
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    console.log(name)
+    console.log(thunkAPI)
+    console.log(thunkAPI.getState())
+    // Allow the use of dispatch regardless of importing
+    // thunkAPI.dispatch(showModal())
+    try {
+      return await axios.get(url).then(({ data }) => data)
+    } catch (err) {
+      console.log(err.response)
+      return thunkAPI.rejectWithValue(
+        "There was an error during your request..."
+      )
+    }
+  }
+)
 
 const cartSlice = createSlice({
   name: "cart",
@@ -48,6 +70,19 @@ const cartSlice = createSlice({
       })
       state.amount = amount
       state.total = total
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.cartItems = action.payload
+      state.isLoading = false
+    },
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action)
+      state.isLoading = false
     },
   },
 })
